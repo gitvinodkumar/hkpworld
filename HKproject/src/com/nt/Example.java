2fd093a426f5ec2f9614ee9d6a155a244229d9f7
Ayyappa <Ayyappa@DESKTOP-2IT6B59>
2018-06-14 12:13:37 +0530

    hk-26

--- HKproject/src/com/nt/Example.java
+++ HKproject/src/com/nt/Example.java
@@ -3,18 +3,19 @@ package com.nt;
 public class Example {
 
 	
-	static void m1(){
+	 int m1(){
 		System.out.println("hii heloo");
+		return 10;
 	}
 	public static void main(String[] args) {
 		
 		System.out.println("hwloo world");
 		System.out.println("hiii mama");
-		Sample e=new Sample();
+		Example e=new Example();
 		Sample23 e1=new Sample23();
 		
 		System.out.println(e.m1());
-		System.out.println(e1.m2());
+	//	System.out.println(e1.m2());
 		
 
 	}
